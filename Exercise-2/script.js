de326999b8c040291a75494e2082bd6fed8ae440
MorganP52 <morganperry52@gmail.com>
2025-01-28 21:38:24 -0400

    Add files via upload

---
+++ Exercise-2/script.js
@@ -0,0 +1,31 @@
+let height=180;
+const userAge=30;
+console.log("Answer on Part One");
+console.log("Height Variable is", height);
+console.log("Age Variable is", userAge);
+
+console.log("Answer For Part 2");
+
+const petName = [
+    "Remy" ,
+    "Rocky" ,
+    "Max"
+];
+console.log("Array After Creating" , petName);
+petName.push("David");
+console.log("Array After Using Push" , petName);
+petName.pop();
+console.log("Array After Using Reverse" , petName);
+
+console.log("Answer For Part 3");
+let car = {
+    Make: "Plymouth" ,
+    Model: "Laser RT" ,
+    Year: "1992"
+};
+console.log("Car Info Is As Follows");
+car.year =1991;
+console.log("Car Info After Updating The Year");
+
+car.color = "Green"
+console.log("Car Info After Adding Colour");
