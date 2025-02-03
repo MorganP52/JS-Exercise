5c82d4a344ba6b8b646be720890abdea6455e9f9
MorganP52 <morganperry52@gmail.com>
2025-02-02 22:47:02 -0400

    Update script.js

--- Exercise 3/script.js
+++ Exercise 3/script.js
@@ -1,69 +1,50 @@
+// Name: Morgan Perry
+// W#:0487849
+// Client Side Programming
+// Assignment: Exercise 3
+// Instructor: Nadia Gouda
+// Date: February 2, 2025
+
 // part 1 in the ex
-
 let Number1 = 10;
-
 let Number2 = 20;
 
 //(+, -,, /, *, %, ) 
-
 console.log("the additionresult", Number1 + Number2);
-
 console.log("the subtraction", Number1 - Number2);
-
 console.log("the multiplication", Number1 * Number2);
-
 console.log("the Division", Number1 / Number2);
-
 console.log("the Mod", Number1 % Number2);
 
 // part 2 in Ex  (>, <, >=, <=, ===, !==).
-
 console.log("Greater than (>) :", Number1 > Number2);
-
 console.log("Less than (<):", Number1 < Number2);
-
 console.log("Greater Than or equal to (<=) :", Number1 <= Number2);
-
 console.log("Less than or equal to (>=) :", Number1 >= Number2);
-
 console.log("Equal (===) :", Number1 === Number2);
-
 console.log("Not Equal (!==) :", Number1 !== Number2);
 
 // part 3 isSunny, isWeekend).
-
 let isSunny = false;
-
 let isWeekend = true;
 
 //Use logical operators (&&, ) to check combinations of conditions.
-
 console.log("is it good for picnic :", isSunny && isWeekend);
-
 console.log("is it sunny or weekend :", isSunny || isWeekend);
 
 // part 4 : Declare a variable score and assign it a value.
-
 let score = 85;
-
 score += 15;
-
 console.log("My score becuase I am better then David :", score);
-
 score -= 85;
-
 console.log("Davids Score because I am better :", score);
 
 //alert box
-
 alert("hello everyone");
 
 // confirm box
-
 confirm("Are you of legal Age  ?")
 
 //propmt
-
 prompt("Please enter your name : ", "Ex: Morgan ");
-
 prompt("Please enter your Age: ", "Ex: 20 ");
